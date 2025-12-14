93e11fe6ee3849de169d2c5625db9cdfb8de0acd
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2025-12-14 14:29:53 +0700

    refactor: separate current funcs into file.go (for a tidier main.go)

---
+++ file.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"strings"
+	"os"
+	"path/filepath"
+)
+
+func isFruuppFile(filePath string) bool {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return false
+	}
+	defer file.Close() // auto called when the func returns
+
+	// scans the opened file (for text file)
+	scanner := bufio.NewScanner(file)
+	if scanner.Scan() {
+		firstLine := scanner.Text()
+		firstLine = strings.TrimSpace(firstLine)
+
+		return firstLine == "#!fruupp"
+	}
+
+	return false
+}
+
+func listFruuppFiles(dirPath string) ([]string, error) {
+	entries, err := os.ReadDir(dirPath)
+	if err != nil { return nil, err }
+
+	var fruuppFiles []string
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			filePath := filepath.Join(dirPath, entry.Name())
+			if isFruuppFile(filePath) {
+				fruuppFiles = append(fruuppFiles, filePath)
+			}
+		}
+	}
+
+	return fruuppFiles, nil
+}
--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
-	"path/filepath"
 )
 
 type Song struct {
@@ -16,46 +13,10 @@ type Song struct {
 	Lyrics string
 }
 
-func isFruuppFile(filePath string) bool {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return false
-	}
-	defer file.Close() // auto called when the func returns
-
-	// scans the opened file (for text file)
-	scanner := bufio.NewScanner(file)
-	if scanner.Scan() {
-		firstLine := scanner.Text()
-		firstLine = strings.TrimSpace(firstLine)
-
-		return firstLine == "#!fruupp"
-	}
-
-	return false
-}
-
-func listFruuppFiles(dirPath string) ([]string, error) {
-	entries, err := os.ReadDir(dirPath)
-	if err != nil { return nil, err }
-
-	var fruuppFiles []string
-	for _, entry := range entries {
-		if !entry.IsDir() {
-			filePath := filepath.Join(dirPath, entry.Name())
-			if isFruuppFile(filePath) {
-				fruuppFiles = append(fruuppFiles, filePath)
-			}
-		}
-	}
-
-	return fruuppFiles, nil
-}
-
 func main() {
 	isIt := isFruuppFile("./lyrics/get-em-out-by-friday")
 	fmt.Printf("%t\n", isIt)
 	files, _ := listFruuppFiles("./lyrics") 
 	fmt.Println(files)
-	fmt.Println(os.Args[1])
+	fmt.Println(os.Args[0])
 }
