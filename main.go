a4eb936cbf2731afde6de998378f4d66dc5c2f6a
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2025-12-17 18:45:53 +0700

    feat: add 'view' command! try to use it.
    syntax: fruupp view <filePath>
    alr contains validation checking (file exist, permission, is a fruupp
    file, etc)

--- main.go
+++ main.go
@@ -15,10 +15,34 @@ func main() {
 		displayHelp()
 	case "version":
 		displayVersion()
-	case "ver":
-		displayVersion()
 	case "list":
 		displayListSongs()
+	case "view":
+		if len(os.Args) >= 3 {
+			filePath := os.Args[2]
+
+			if !isFruuppFile(filePath) {
+				displayError("Not a fruupp file.")
+				os.Exit(1)
+			}
+
+			song, err := parseFruuppFile(filePath)
+			if err != nil {
+				if os.IsNotExist(err) {
+					// specific error: file doesnt exist
+					displayError("File doesn't exist.")
+				} else {
+					// any other error: perm denied, scanning error, etc
+					displayError(err.Error())
+				}
+				os.Exit(1)
+			}
+
+			displaySong(song)
+		} else {
+			displayError("No file provided.")
+			os.Exit(1)
+		}
 	default:
 		displayError("Unknown command.")
 		os.Exit(1)
